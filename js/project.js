bebd57ee0bc1e4ff5ccc41a6a8c827ee52d09de6
Nik Tarasenko <nik.d.tarasenko@gmail.com>
2017-10-10 22:44:07 +0300

    - 020

--- js/project.js
+++ js/project.js
@@ -992,23 +992,27 @@ $(document).ready(function () {
         $thisTr.remove();
 
     });
-
+	
+	
+	
 	//
 	//
 	//CHECK A ANSWER
 	//
 	//
 	
-	//By Nik
+	var $questionSet = [{id: 1, answer: '000'}]
+	$("#id").hide();
+	
 	$("#checkAnswer").click(function (e) {
-        e.preventDefault();
-        client_answer = $("#clientAnswer").val();
-		answer = $("#answer").val();
-        //client_comment = $("#new_client_comment").val();
-        if (!client_answer || client_answer == "") {
-            alert("А где ответ?");
-            return;
-        }
+		e.preventDefault();
+		client_answer = $("#clientAnswer").val();
+		answer = $questionSet[$("#id").val()].answer;
+		//client_comment = $("#new_client_comment").val();
+		if (!client_answer || client_answer == "") {
+			alert("А где ответ?");
+			return;
+		}
 		if (client_answer == answer){
 			alert("Верно!");
 			return;
@@ -1017,11 +1021,11 @@ $(document).ready(function () {
 			alert("Не верно! Подумай еще.");
 			return;
 		}
-    });
-	
-	$(document).ready(function() {
-		$("#answer").hide();
 	});
+	
+	
+	
+	
 	
     //
     //
