bb3842fed7883bcefbbee47d3686fd9492373b82
Nik Tarasenko <nik.d.tarasenko@gmail.com>
2017-10-10 21:01:54 +0300

    - 003

--- main2.py
+++ main2.py
@@ -3,9 +3,13 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
 from google.appengine.api import users
 
+import os
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
-	self.response.write(
-            '<html><body>{}</body></html>'.format(greeting))
+	template_values = {
+            'sometext': "Some text for test"}
+        path = os.path.join(os.path.dirname(__file__), 'foo.html')
+        self.response.out.write(template.render(path, template_values))
 		
 application = webapp2.WSGIApplication([('/', MainPage)])
